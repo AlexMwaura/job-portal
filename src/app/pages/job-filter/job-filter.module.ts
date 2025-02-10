3efd3805226fe82d2975914ca7b377683091f444
Alex Mwaura <alexmwaura36@gmail.com>
2025-02-10 12:36:42 +0300

    create job list and job filter components

---
+++ src/app/pages/job-filter/job-filter.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JobFilterComponent } from './job-filter.component';
+
+
+
+@NgModule({
+  declarations: [JobFilterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class JobFilterModule { }
