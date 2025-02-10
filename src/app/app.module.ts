0876b098082f73fedf065ad6b0b453168bc162c0
Alex Mwaura <alexmwaura36@gmail.com>
2025-02-10 11:57:04 +0300

    create job list and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { JobListComponent } from './pages/job-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    JobListComponent,
   ],
   imports: [
     BrowserModule,
