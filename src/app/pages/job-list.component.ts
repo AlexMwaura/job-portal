adcdb0d67415672932b9e7a99af028096be920dd
Alex Mwaura <alexmwaura36@gmail.com>
2025-02-10 12:10:10 +0300

    create job list component

--- src/app/pages/job-list.component.ts
+++ src/app/pages/job-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-job-list',
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.scss'
 })
-export class JobListComponent {
+export class JobListComponent implements OnInit {
+  jobs: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('assets/jobs.json').subscribe(data => {
+      this.jobs = data;
+    });
+  }
 }
