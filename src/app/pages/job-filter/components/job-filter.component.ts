7aee1e48a13770c7f1032f6cbb81622869ec2d28
Alex Mwaura <alexmwaura36@gmail.com>
2025-02-12 15:47:11 +0300

    modify the json file and also move the job filter components

---
+++ src/app/pages/job-filter/components/job-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-filter',
+  templateUrl: './job-filter.component.html',
+  styleUrl: './job-filter.component.scss'
+})
+export class JobFilterComponent {
+
+}
