ae6d014260278816711e4af7969ea23d4cbfae42
Alex Mwaura <alexmwaura36@gmail.com>
2025-03-20 16:40:47 +0300

    create job details component and implement logic to fetch job job details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/components/home.component';
+import { JobDetailsComponent } from './pages/job-details/components/job-details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route,
+  { path: 'job/:id', component: JobDetailsComponent }, // Route for job details
   { path: '**', redirectTo: '' },
 ];
 
